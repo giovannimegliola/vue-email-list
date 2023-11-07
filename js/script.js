8568e10cbc6d0707cdee9542842d98f49220f7ff
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-07 15:37:18 +0100

    printed random email list

--- js/script.js
+++ js/script.js
@@ -3,9 +3,22 @@ const { createApp } = Vue;
 createApp ({
   data (){
     return {
-      
+      emailList: [],
     }
-  }
+  },
+  methods: {
+
+  },
+  mounted(){
+
+    for(let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=> {
+          this.email = resp.data.response;
+          this.emailList.push(this.email);
+      });
+    }   
+ },
+  
 }
 
 ).mount("#app");
